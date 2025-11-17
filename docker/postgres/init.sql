d5d500726e32165775bc2ea41c7f73d704cd34f1
Wolfiler <erwann.large@laposte.net>
2025-11-17 16:22:12 +0100

    Updated docker init.sql script with postgres database scheme

---
+++ docker/postgres/init.sql
@@ -0,0 +1,46 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+CREATE TABLE external_user(
+    id UUID PRIMARY KEY,
+    user_id varchar(255) NOT NULL UNIQUE,
+    username varchar(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE content(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    creator_id UUID NOT NULL,
+    parent_id UUID default NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    text TEXT NOT NULL,
+    media_urls JSONB default NULL,
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (parent_id) REFERENCES content(id),
+    FOREIGN KEY (creator_id) REFERENCES external_user(id)
+);
+
+CREATE TABLE user_content_like(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    content_id UUID NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES external_user(id),
+    FOREIGN KEY (content_id) REFERENCES content(id),
+    CONSTRAINT uc_user_content_like UNIQUE (user_id, content_id)
+);
+
+CREATE TABLE user_content_favorite(
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id UUID NOT NULL,
+  content_id UUID NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES external_user(id),
+  FOREIGN KEY (content_id) REFERENCES content(id),
+  CONSTRAINT uc_user_content_favorite UNIQUE (user_id, content_id)
+);
+
+CREATE INDEX idx_content_creator ON content(creator_id);
+CREATE INDEX idx_comments ON content(parent_id) WHERE parent_id IS NOT NULL;
+CREATE INDEX idx_posts ON content(parent_id) WHERE parent_id IS NULL;
+CREATE INDEX idx_user_content_like ON user_content_like(user_id, content_id);
+CREATE INDEX idx_user_content_favorite ON user_content_favorite(user_id, content_id);
